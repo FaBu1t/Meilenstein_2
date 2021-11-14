84103fd044db7ec1c6136ec102fe5c8323fe3ff3
fbuitkam <fabian.buitkamp@hs-osnabrueck.de>
2021-11-14 18:57:22 +0100

    added methods in DataService

--- Angula/src/app/app.component.ts
+++ Angula/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -16,103 +17,45 @@ export class AppComponent {
     'rating': ''
   };
 
+  movieArray = []
 
-  sample_movie_one = {
-    'title': 'Avengers',
-    'release': '19.12.2001',
-    'length': '164 Minutes',
-    'regisseur': 'Peter Elcron',
-    'rating': '5/5'
-  }
-
-  movieArray = [
-    {
-      'title': 'Avengers',
-      'release': '19.12.2001',
-      'length': '164 Minutes',
-      'regisseur': 'Peter Elcron',
-      'rating': '5/5'
-    },
-    {
-      'title': 'Dune',
-      'release': '19.09.2021',
-      'length': '155 Minutes',
-      'regisseur': 'Denis Villeneuve',
-      'rating': '5/5'
-    },
-    {
-      'title': 'Jupiter Ascendingengers',
-      'date': '09.11.2015',
-      'length': 'Zu Lang',
-      'regisseur': 'Schlechter Film',
-      'rating': '1'
-    },
-    {
-      'title': 'Der Schatzplanet',
-      'date': '05.03.2002',
-      'length': '95',
-      'regisseur': 'Jon Clements',
-      'rating': '5'
-    },
-    {
-      'title': 'Pacific Rim',
-      'date': '18.07.2021',
-      'length': '131',
-      'regisseur': 'Guillermo del Toro',
-      'rating': '5'
-    }
-  ]
-
-  constructor()
-  {
-    if (localStorage.getItem('persons'))
-    {
-      this.movieArray = JSON.parse(localStorage.getItem('persons'));
-    }else{
-      console.log(this.movieArray);
-    }
-  }
-
-  change()
+  constructor(private dataService:DataService)
   {
     
   }
 
-  add()
+  ngOnInit(){
+    this.getMovies();
+  }
+
+
+  getMovies(){
+    this.movieArray=this.dataService.getMovies();
+  }
+
+
+  change()
   {
-    this.movieArray.push({
-      'title': this.movie.title,
-      'release': this.movie.release,
-      'length': this.movie.length,
-      'regisseur': this.movie.regisseur,
-      'rating': this.movie.rating
-    });
-    this.movie.title = '';
-    this.movie.release = '';
-    this.movie.length = '';
-    this.movie.release= '';
-    this.movie.release= '';
-    this.persist();
+   
+  }
+
+  add(movie)
+  {
+    this.movie=this.dataService.add(movie);
   }
 
   edit(movie)
   {
     this.movie = movie;
+
   }
 
   delete(movie)
   {
-    this.movieArray.splice(this.movieArray.indexOf(movie), 1);
-    this.persist();
-  }
-
-  persist()
-  {
-    localStorage.setItem('persons', JSON.stringify(this.movieArray));
+      this.dataService.delete(movie);
   }
 
   deleteLocalstorage(){
-    localStorage.clear();
-    location.reload();
+    this.dataService.deleteLocalstorage();
   }
 }
--- Angula/src/app/data.service.ts
+++ Angula/src/app/data.service.ts
@@ -5,5 +5,98 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
 
-  constructor() { }
+
+  movieArray = [
+    {
+      'title': 'Avengers',
+      'release': '19.12.2001',
+      'length': '164 Minutes',
+      'regisseur': 'Peter Elcron',
+      'rating': '5/5'
+    },
+    {
+      'title': 'Dune',
+      'release': '19.09.2021',
+      'length': '155 Minutes',
+      'regisseur': 'Denis Villeneuve',
+      'rating': '5/5'
+    },
+    {
+      'title': 'Jupiter Ascendingengers',
+      'date': '09.11.2015',
+      'length': 'Zu Lang',
+      'regisseur': 'Schlechter Film',
+      'rating': '1'
+    },
+    {
+      'title': 'Der Schatzplanet',
+      'date': '05.03.2002',
+      'length': '95',
+      'regisseur': 'Jon Clements',
+      'rating': '5'
+    },
+    {
+      'title': 'Pacific Rim',
+      'date': '18.07.2021',
+      'length': '131',
+      'regisseur': 'Guillermo del Toro',
+      'rating': '5'
+    }
+  ]
+
+
+  constructor() { 
+    if (localStorage.getItem('persons'))
+    {
+      this.movieArray = JSON.parse(localStorage.getItem('persons'));
+    }else{
+      console.log(this.movieArray);
+    }
+  }
+
+
+  add(movie)
+  {
+    this.movieArray.push({
+      'title': movie.title,
+      'release': movie.release,
+      'length': movie.length,
+      'regisseur': movie.regisseur,
+      'rating': movie.rating
+    });
+    movie.title = '';
+    movie.release = '';
+    movie.length = '';
+    movie.regisseur= '';
+    movie.rating= '';
+    this.persist();
+
+    return movie;
+  }
+
+  edit(movie)
+  {
+
+  }
+
+
+  getMovies() {
+    return this.movieArray;
+  }
+
+  delete(movie){
+    this.movieArray.splice(this.movieArray.indexOf(movie), 1);
+    this.persist();
+  }
+
+  persist()
+  {
+    localStorage.setItem('persons', JSON.stringify(this.movieArray));
+  }
+
+  deleteLocalstorage(){
+    localStorage.clear();
+    location.reload();
+  }
+
 }
